4f8373e582ceea2e854ffbdd04ce0e5224602d5b
ayham-svg <hezeirinya@gmail.com>
2025-03-25 13:13:58 +0100

    d

--- webstite-portfolio/POTFOLIO/script.js
+++
@@ -1,9 +0,0 @@
-// Selecteer de knop en het menu
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-// Voeg een klik-event toe aan de hamburger-knop
-menuToggle.addEventListener('click', function () {
-    // Toggle de 'show' klasse van het menu
-    menu.classList.toggle('show');
-});
